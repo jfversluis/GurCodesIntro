05fd0e3f5a0fa4d0d93c89b013072f7ae5444acf
Gerald Versluis <gerald@verslu.is>
2017-05-11 19:41:44 +0200

    Initial commit

---
+++ GurCodesIntro/GurCodesIntroPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GurCodesIntro
+{
+    public partial class GurCodesIntroPage : TabbedPage
+    {
+        public GurCodesIntroPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GurCodesIntro/Tab2Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GurCodesIntro
+{
+    public partial class Tab2Page : ContentPage
+    {
+        public Tab2Page()
+        {
+            InitializeComponent();
+
+            MyListView.ItemsSource = new List<string>
+            {
+                "First",
+                "Second",
+                "Foo",
+                "Unicorn"
+            };
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Button clicked", "Hello World!", "OK");
+        }
+    }
+}
